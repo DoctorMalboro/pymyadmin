ef950474c2e733df6d118874621ea62dcda48655
DoctorMalboro <leandro@leandropoblet.com>
2013-06-20 17:09:20 -0300

    Redirects base url to admin

--- pymyadmin/pma.py
+++ pymyadmin/pma.py
@@ -2,10 +2,16 @@
 
 from flask import Flask
 from pymyadmin.admin import setup_admin_for
+from flask import redirect
 
 
 app = Flask(__name__)
 setup_admin_for(app)
 
+# Redirects the site straight to the administration panel
+
+@app.route('/')
+def index():
+    return redirect('/admin/')
 
 app.run(debug=True)
